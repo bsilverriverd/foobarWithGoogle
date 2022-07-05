142df1efbae65b76c8125b9f4eef7c7f5d6eef22
bsilverriverd <edl1111@naver.com>
2022-07-06 03:14:56 +0900

    challenge4-1: passes Test 1,2,4,5

--- challenge4-1/distractTheTrainers.py
+++ challenge4-1/distractTheTrainers.py
@@ -1,2 +1,58 @@
 def solution(banana_list):
+	adjacencyList = {}
+	for i in range(len(banana_list)):
+		adjacencyList[i] = []
 
+	for i in range(len(banana_list)):
+		for j in range(len(banana_list)):
+			mean = (banana_list[i] + banana_list[j]) // 2
+			if banana_list[i] == banana_list[j]:
+				pass
+			elif (banana_list[i] + banana_list[j]) % 2 == 1:
+				adjacencyList[i].append(j)
+			elif mean % 2 == 1:
+				adjacencyList[i].append(j)
+			else:
+				less = min(banana_list[i], banana_list[j])
+				while less < mean:
+					less = less * 2
+				if less != mean:
+					adjacencyList[i].append(j)
+
+	degree = {}
+	for vertex in adjacencyList:
+		degree[vertex] = len(adjacencyList[vertex])
+
+	while adjacencyList:
+		minDegree = min(degree.values())
+
+		if minDegree == 0:
+			break
+
+		minDegreeVertex = min(degree, key=degree.get)
+		neighborMinDegreeVertex = adjacencyList[minDegreeVertex][0]
+		neighborMinDegree = degree[neighborMinDegreeVertex]
+
+		for neighbor in adjacencyList[minDegreeVertex][1:]:
+			if degree[neighbor] < neighborMinDegree:
+				neighborMinDegree = degree[neighbor]
+				neighborMinDegreeVertex = neighbor
+
+		del adjacencyList[minDegreeVertex]
+		del adjacencyList[neighborMinDegreeVertex]
+		del degree[minDegreeVertex]
+		del degree[neighborMinDegreeVertex]
+
+		for vertex in adjacencyList:
+			if minDegreeVertex in adjacencyList[vertex]:
+				adjacencyList[vertex].remove(minDegreeVertex)
+				degree[vertex] = degree[vertex] - 1
+			if neighborMinDegreeVertex in adjacencyList[vertex]:
+				adjacencyList[vertex].remove(neighborMinDegreeVertex)
+				degree[vertex] = degree[vertex] - 1
+
+	return len(adjacencyList)
+
+#print(solution([1, 1]))
+#print(solution([1, 7, 3, 21, 13, 19]))
+#print(solution([1]))
